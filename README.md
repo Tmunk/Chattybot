beae13ec95b2b3206ac8ef86323811601cfed73a
Thomas Jarv Munk <tmunk72@gmail.com>
2021-09-13 01:51:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chattybot
+ Maple Chat Bot
---
+++ src/com/bot/Main.java
@@ -0,0 +1,15 @@
+package com.bot;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleBot bot1 = new SimpleBot();
+        bot1.greet("Maple", "2021"); // change it as you need
+        bot1.remindName();
+        bot1.guessAge();
+        bot1.count();
+        bot1.test();
+        bot1.end();
+    }
+}
